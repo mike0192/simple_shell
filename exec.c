1b8627dab0aecfe9a27d84d885ee628ccf818171
michael <michaelhagos163@gmail.com>
2023-10-18 03:54:15 -0700

    s

--- exec.c
+++ exec.c
@@ -14,21 +14,18 @@ int _execute(char **command, char **envp)
 	son = fork();
 	if (son == 0)
 	{
-		if (son == 0)
-		{
 			if (execve(command[0], command, envp) == -1)
 			{
 				perror(envp[0]);
 				freearray2D(command);
 				exit(0);
 			}
-		}
+	}
 		else
 		{
 			waitpid(son, &status, 0);
 			freearray2D(command);
 		}
 		return (WEXITSTATUS(status));
-	}
-	return (0);
+	
 }
